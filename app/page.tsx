03961739a736ce993f69843955c36fa446f245b0
OrcDev <urosmiric88@gmail.com>
2025-10-29 15:30:42 +0100

    style(app) wrap SubmitProjectForm in a main container for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { SubmitProjectForm } from "@/components/forms/submit-project-form";
 
 export default function Home() {
-  return <SubmitProjectForm />;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <SubmitProjectForm />
+    </main>
+  );
 }
