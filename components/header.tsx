bc343c152aa9d3fda27d9d52c73f87bdc0e44de3
OrcDev <urosmiric88@gmail.com>
2025-10-29 16:34:15 +0100

    feat(app) add orcdev logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import { ModeSwitcher } from "./mode-switcher";
 
 export function Header() {
   return (
     <header className="absolute top-0 flex w-full items-center justify-between p-4">
-      <h1>OrcDev Project Review</h1>
+      <div className="flex items-center gap-2">
+        <Image
+          alt="OrcDev Project Review"
+          height={32}
+          src="/orcdev.png"
+          width={32}
+        />
+        <h1>OrcDev Project Review</h1>
+      </div>
       <ModeSwitcher />
     </header>
   );
