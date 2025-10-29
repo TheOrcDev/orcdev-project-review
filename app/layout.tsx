66ba3fdb64ae45c7db47a9797562edaa3cc4b30b
OrcDev <urosmiric88@gmail.com>
2025-10-29 16:44:54 +0100

    feat(app) add Footer component and integrate it into the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -39,6 +40,7 @@ export default function RootLayout({
           <Header />
           {children}
           <Toaster />
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
