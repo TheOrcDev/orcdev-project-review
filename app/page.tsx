98ac8982c866d096f370dbc36bf45e36424b90b1
OrcDev <urosmiric88@gmail.com>
2025-10-29 16:34:53 +0100

    style(app) adjust padding in main container for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { SubmitProjectForm } from "@/components/forms/submit-project-form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center px-5">
       <SubmitProjectForm />
     </main>
   );
