a0f3580bb09edf716b5f6953f46046db697d734c
OrcDev <urosmiric88@gmail.com>
2025-10-29 17:57:58 +0100

    feat(header) replace header title with a link to the homepage for improved navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 import { ModeSwitcher } from "./mode-switcher";
 
 export function Header() {
   return (
     <header className="absolute top-0 flex w-full items-center justify-between p-4">
-      <div className="flex items-center gap-2">
+      <Link
+        className="flex items-center gap-2"
+        href="/"
+        rel="noopener noreferrer"
+        title="OrcDev Project Review"
+      >
         <Image
           alt="OrcDev Project Review"
           height={32}
           src="/orcdev.png"
           width={32}
         />
-        <h1>OrcDev Project Review</h1>
-      </div>
+      </Link>
       <ModeSwitcher />
     </header>
   );
