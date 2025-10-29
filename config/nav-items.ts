a0fd3889772c22bbd7b5cb76dc43061f3ba82364
OrcDev <urosmiric88@gmail.com>
2025-10-29 18:12:13 +0100

    chore: latest dependencies

--- config/nav-items.ts
+++ config/nav-items.ts
@@ -1,6 +1,10 @@
 export const navItems = {
   header: [
     {
+      label: "Submit Project",
+      href: "/submit-project",
+    },
+    {
       label: "Livestreams",
       href: "https://www.youtube.com/@orcdev/streams",
       target: "_blank",
