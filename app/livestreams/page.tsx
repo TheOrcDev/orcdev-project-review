aff622ca893263807aff11033b71bb111b4484e0
OrcDev <urosmiric88@gmail.com>
2025-10-30 23:17:57 +0100

    feat(livestreams) enhance livestreams page layout and add embedded video for November 2025 event

--- app/livestreams/page.tsx
+++ app/livestreams/page.tsx
@@ -7,10 +7,27 @@ export default function Livestreams() {
       <Link href="/">
         <Button>Back</Button>
       </Link>
-      <h1 className="font-bold text-2xl">OrcDev Project Review Livestreams</h1>
-      <p className="text-center text-muted-foreground text-xs">
-        All livestreams will be listed here.
+      <h1 className="text-center font-bold md:text-2xl">
+        OrcDev Project Review Livestreams
+      </h1>
+      <p className="text-center text-muted-foreground text-xs md:text-sm">
+        All livestreams are listed here.
       </p>
+
+      <div className="mt-5 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-center font-bold md:text-2xl">
+            4th November 2025
+          </h2>
+          <div className="flex justify-center">
+            <iframe
+              className="aspect-video w-full"
+              src={"https://www.youtube.com/embed/oaD2svrWWnU"}
+              title="OrcDev Project Review Livestream"
+            />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
