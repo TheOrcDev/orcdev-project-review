b058b4dcb935f01a7a9c60c1bb87de8b13b75557
OrcDev <urosmiric88@gmail.com>
2025-11-06 11:08:35 +0100

    feat(livestreams): add new livestream event for November 11, 2025, and adjust heading size for consistency

--- app/livestreams/page.tsx
+++ app/livestreams/page.tsx
@@ -16,13 +16,26 @@ export default function Livestreams() {
 
       <div className="mt-5 grid grid-cols-1 gap-4 lg:grid-cols-2">
         <div className="flex flex-col gap-4">
-          <h2 className="text-center font-bold md:text-2xl">
+          <h2 className="text-center font-bold md:text-xl">
             4th November 2025
           </h2>
           <div className="flex justify-center">
             <iframe
               className="aspect-video w-full"
               src={"https://www.youtube.com/embed/oaD2svrWWnU"}
+              title="OrcDev Project Review Livestream"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-4">
+          <h2 className="text-center font-bold md:text-xl">
+            11th November 2025
+          </h2>
+          <div className="flex justify-center">
+            <iframe
+              className="aspect-video w-full"
+              src={"https://www.youtube.com/embed/EG45hfpIAoc"}
               title="OrcDev Project Review Livestream"
             />
           </div>
