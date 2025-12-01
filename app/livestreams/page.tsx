64054c1e028c903fbea250a3279d9de63611dee7
OrcDev <urosmiric88@gmail.com>
2025-12-01 09:21:15 +0100

    feat(livestreams): add new livestream event for December 2, 2025

--- app/livestreams/page.tsx
+++ app/livestreams/page.tsx
@@ -18,6 +18,10 @@ const livestreams = [
     date: "26th November 2025",
     url: "https://www.youtube.com/live/w4AUms5wNpY",
   },
+  {
+    date: "2nd December 2025",
+    url: "https://www.youtube.com/live/Q1JyVbDoQSM",
+  },
 ];
 
 export default function Livestreams() {
