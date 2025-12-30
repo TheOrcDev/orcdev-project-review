0da6da4e29fec339416dc77c78b06482905e06fb
OrcDev <urosmiric88@gmail.com>
2025-12-30 21:30:54 +0100

    feat(livestreams): add new livestream event for December 30, 2025

--- app/livestreams/page.tsx
+++ app/livestreams/page.tsx
@@ -3,6 +3,10 @@ import { Button } from "@/components/ui/8bit/button";
 
 const livestreams = [
   {
+    date: "30th December 2025",
+    url: "https://www.youtube.com/live/m7OgBhY36tU",
+  },
+  {
     date: "23rd December 2025",
     url: "https://www.youtube.com/live/bJ9wuU0Jj6g",
   },
