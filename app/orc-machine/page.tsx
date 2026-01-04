6e72368cb872972e34443171ed3266e86c306c01
OrcDev <urosmiric88@gmail.com>
2026-01-04 15:53:58 +0100

    style(orc-machine): enhance layout of project picker explanation with improved styling

--- app/orc-machine/page.tsx
+++ app/orc-machine/page.tsx
@@ -11,23 +11,26 @@ export default function Home() {
       <Button onClick={deleteAllProjectsAndAddToReviewedProjects}>
         Delete All Projects and Add to Reviewed Projects
       </Button> */}
-      <h1 className="text-center font-bold">
-        The Orc Machine is sleeping (for now)
-      </h1>
-      <p className="text-xs">
-        The Orc Machine is our random project picker for the livestream. When
-        it’s awake, it grabs one project from the submitted list and that’s what
-        gets reviewed — no favoritism, just the wheel of fate.
-      </p>
+      <div className="flex flex-col gap-3 border border-dashed p-4">
+        <h1 className="text-center font-bold">
+          The Orc Machine is sleeping (for now)
+        </h1>
+        <p className="text-xs">
+          The Orc Machine is our random project picker for the livestream. When
+          it’s awake, it grabs one project from the submitted list and that’s
+          what gets reviewed — no favoritism, just the wheel of fate.
+        </p>
 
-      <p className="text-xs">
-        Want better odds? We also pick one project from the Discord community
-        each stream, so joining gives you an extra chance to get pulled. Hop in:{" "}
-        <Link className="underline" href="https://join.thehorde.dev/">
-          Join the Horde
-        </Link>
-        !
-      </p>
+        <p className="text-xs">
+          Want better odds? We also pick one project from the Discord community
+          each stream, so joining gives you an extra chance to get pulled. Hop
+          in:{" "}
+          <Link className="underline" href="https://join.thehorde.dev/">
+            Join the Horde
+          </Link>
+          !
+        </p>
+      </div>
     </main>
   );
 }
