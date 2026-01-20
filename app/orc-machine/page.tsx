79374ad69f8843286ce783fd1cdc39966aa77200
OrcDev <urosmiric88@gmail.com>
2026-01-20 21:38:26 +0100

    fix: orc machine

--- app/orc-machine/page.tsx
+++ app/orc-machine/page.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
-import { PickProject } from "@/components/pick-project";
 import { Button } from "@/components/ui/8bit/button";
-import { deleteAllProjectsAndAddToReviewedProjects } from "@/server/projects";
 
 export default function Home() {
   return (
@@ -9,11 +7,11 @@ export default function Home() {
       <Link href="/">
         <Button variant="outline">Back</Button>
       </Link>
-      <PickProject />
+      {/* <PickProject /> */}
       {/* <RandomNumber /> */}
-      <Button onClick={deleteAllProjectsAndAddToReviewedProjects}>
+      {/* <Button onClick={deleteAllProjectsAndAddToReviewedProjects}>
         Delete All Projects and Add to Reviewed Projects
-      </Button>
+      </Button> */}
       {/* <div className="flex flex-col gap-3 border border-dashed p-4">
         <h1 className="text-center font-bold">
           The Orc Machine is sleeping (for now)
